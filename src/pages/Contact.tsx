f34095fdd19a5a90df0b95187e5aaad5fd15900f
hoshiirui <shibaharumii@gmail.com>
2025-01-05 09:21:41 +0800

    completing contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -106,12 +106,93 @@ export default function Contact() {
               Instagram
             </button> */}
           </div>
+          {/* form */}
+          <div className="col-span-1 p-8 bg-white shadow-lg rounded-xl grid grid-cols-2 gap-4">
+            {/* <p className="text-2xl font-bold text-sarkara-sign">
+              Informasi Kontak
+            </p>
+            <p className="text-sm text-stone-400">
+              Jika Anda mempunyai pertanyaan atau kekhawatiran, Anda dapat
+              menghubungi kami di kontak yang tertera berikut:
+            </p> */}
+            <div className="col-span-full lg:col-span-1">
+              <label
+                htmlFor="name"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Nama
+              </label>
+              <div className="mt-2">
+                <div className="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-sarkara-sign-1">
+                  <input
+                    id="name"
+                    name="name"
+                    type="text"
+                    placeholder=""
+                    className="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col-span-full lg:col-span-1">
+              <label
+                htmlFor="email"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Email
+              </label>
+              <div className="mt-2">
+                <div className="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-sarkara-sign-1">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    placeholder=""
+                    className="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col-span-full">
+              <label
+                htmlFor="pesan"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Pesan
+              </label>
+              <div className="mt-2">
+                <textarea
+                  id="pesan"
+                  name="pesan"
+                  rows={3}
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-sarkara-sign-1 sm:text-sm/6"
+                  defaultValue={""}
+                />
+              </div>
+              <p className="mt-3 text-sm/6 text-gray-600">
+                Tulis beberapa kalimat pesan untuk kami.
+              </p>
+            </div>
+            <div className="mt-6 flex items-center justify-end gap-x-6 col-span-full">
+              <button
+                type="button"
+                className="text-sm/6 font-semibold text-gray-900"
+              >
+                Kosongkan
+              </button>
+              <button
+                type="submit"
+                className="rounded-md bg-sarkara-sign px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sarkara-sign-1 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sarkara-sign-1"
+              >
+                Kirim Pesan
+              </button>
+            </div>
+          </div>
         </div>
         <div className="flex items-center justify-center flex-col mb-8 mt-24 lg:mb-12 gap-3">
           <p className="text-3xl text-sarkara-sign font-bold">Lokasi Kami</p>
           <p className="text-slate-400 text-sm">
-            Ada kritik atau saran? Langsung hubungi kami melalui kontak di
-            bawah!
+            Temukan dan kunjungi kami di lokasi berikut!
           </p>
         </div>
         <div>
@@ -120,7 +201,7 @@ export default function Contact() {
             width="100%"
             height="450"
             style={{ border: 0 }}
-            allowFullScreen=""
+            allowFullScreen={true}
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
