99eb3849aa1374a6589018e2686dd673dfc04991
hoshiirui <shibaharumii@gmail.com>
2025-01-09 17:07:04 +0800

    menu card and link to ig in homepage

--- src/components/ProductModal.tsx
+++ src/components/ProductModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Dialog,
   DialogBackdrop,
@@ -16,7 +16,7 @@ import {
   PlusCircleIcon,
   StarIcon,
 } from "@heroicons/react/20/solid";
-import { Clock } from "lucide-react";
+import { Circle, CircleDot, Clock, FlameIcon, Snowflake } from "lucide-react";
 import { ClockIcon } from "@heroicons/react/24/solid";
 import { formatToIDR } from "@/helper/idrFormatter";
 import { CurrencyDollarIcon } from "@heroicons/react/20/solid";
@@ -31,9 +31,23 @@ export default function ProductModal({
   onClose: () => void;
   product: MenuSarkara;
 }) {
-  const [caraP, setCaraP] = useState(false);
-  const [syarat, setSyarat] = useState(false);
-  const [menuB, setMenuB] = useState(false);
+  const [pilihanSelected, setPilihanSelected] = useState("");
+  const [variantChoice, setVariantChoice] = useState("");
+  const [priceAdd, setPriceAdd] = useState(0);
+
+  useEffect(() => {
+    setPilihanSelected(
+      product.categories.length === 0
+        ? "none"
+        : product.categories.includes("ice")
+        ? "dingin"
+        : "panas"
+    );
+    if (product.variants) {
+      setVariantChoice(product.variants[0].name);
+    }
+    setPriceAdd(0);
+  }, [isVisible]);
 
   const handleClose = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     // setSyarat(false);
@@ -85,23 +99,126 @@ export default function ProductModal({
                   src={`images/products/${product.menuType}/${product.imageSrc}`}
                   className="aspect-[2/3] w-full rounded-lg bg-gray-100 object-cover sm:col-span-4 lg:col-span-5"
                 />
-                <div className="sm:col-span-8 lg:col-span-7">
-                  <h2 className="text-3xl leading-10 font-bold text-gray-900 sm:pr-12">
-                    {product.name}
-                  </h2>
-                  <p className="text-sm font-medium text-gray-900 text-justify mt-4">
-                    {product.productDetail}
-                  </p>
-                  <p className="text-sm font-medium text-gray-900 text-justify mt-4">
-                    {product.price}
-                  </p>
+                <div className="sm:col-span-8 lg:col-span-7 flex flex-col justify-between h-full">
+                  <div className="">
+                    <h2 className="text-3xl leading-10 font-bold text-gray-900 sm:pr-12">
+                      {product.name}
+                    </h2>
+                    <p className="text-sm font-medium text-gray-900 text-justify mt-4">
+                      {product.productDetail}
+                    </p>
+                    <section className="border-b-4 border-stone-100 pb-5">
+                      <p className="text-md font-bold text-sarkara-sign-1 text-justify mt-4">
+                        Harga:
+                      </p>
+                      <p className="text-lg font-medium text-gray-900 text-justify mt-1">
+                        {formatToIDR(product.price)}
+                      </p>
+                    </section>
+                    {pilihanSelected === "dingin" ||
+                    pilihanSelected === "panas" ? (
+                      <section>
+                        <p className="text-md font-bold text-sarkara-sign-1 text-justify mt-4">
+                          Pilihan Tersedia
+                        </p>
+                        <div className="flex flex-row gap-3 mt-3">
+                          <div
+                            onClick={() => setPilihanSelected("dingin")}
+                            className={`flex flex-col rounded-lg border-2 cursor-pointer ${
+                              pilihanSelected === "dingin"
+                                ? "border-sarkara-sign-2 bg-sarkara-sign-4"
+                                : "border-stone-200 bg-white"
+                            } items-center px-4 py-2 min-w-lg `}
+                          >
+                            <Snowflake
+                              className={`${
+                                pilihanSelected === "dingin"
+                                  ? "text-sarkara-sign-1"
+                                  : "text-stone-300"
+                              }`}
+                            />
+                            <p
+                              className={`font-bold ${
+                                pilihanSelected === "dingin"
+                                  ? "text-sarkara-sign-1"
+                                  : "text-stone-500"
+                              } mt-1`}
+                            >
+                              Dingin
+                            </p>
+                            <p className="text-xs">Cold</p>
+                          </div>
+                          <div
+                            onClick={() => setPilihanSelected("panas")}
+                            className={`flex flex-col rounded-lg border-2 cursor-pointer ${
+                              pilihanSelected === "panas"
+                                ? "border-sarkara-sign-2 bg-sarkara-sign-4"
+                                : "border-stone-200 bg-white"
+                            } items-center px-4 py-2 min-w-lg `}
+                          >
+                            <FlameIcon
+                              className={`${
+                                pilihanSelected === "panas"
+                                  ? "text-sarkara-sign-1"
+                                  : "text-stone-300"
+                              }`}
+                            />
+                            <p
+                              className={`font-bold ${
+                                pilihanSelected === "panas"
+                                  ? "text-sarkara-sign-1"
+                                  : "text-stone-500"
+                              } mt-1`}
+                            >
+                              Panas
+                            </p>
+                            <p className="text-xs">Hot</p>
+                          </div>
+                        </div>
+                      </section>
+                    ) : null}
 
+                    {product.variants && (
+                      <section>
+                        <p className="text-md font-bold text-sarkara-sign-1 text-justify mt-4">
+                          Varian Tersedia
+                        </p>
+
+                        <div className="flex flex-col gap-2 mt-2">
+                          {product.variants.map((variant: any) => (
+                            <div
+                              onClick={() => {
+                                setVariantChoice(variant.name);
+                                setPriceAdd(variant.add);
+                              }}
+                              className="flex flex-row justify-between items-center capitalize cursor-pointer"
+                              key={variant.name}
+                            >
+                              <p className="text-sm">{variant.name}</p>
+                              <div className="flex flex-row gap-2 items-center">
+                                <p className="text-sm">
+                                  + {formatToIDR(variant.add)}
+                                </p>
+                                {variant.name === variantChoice ? (
+                                  <CircleDot className="w-5" />
+                                ) : (
+                                  <Circle className="w-5" />
+                                )}
+                              </div>
+                            </div>
+                          ))}
+                        </div>
+                      </section>
+                    )}
+                  </div>
                   <section
                     aria-labelledby="syarat-dan-ketentuan"
                     className="mt-6"
                   >
                     {/* Cara penggunaan */}
-
+                    <button className="bg-sarkara-sign-1 w-full py-3 rounded-full text-white font-bold">
+                      Harga Total ・ {formatToIDR(product.price + priceAdd)}
+                    </button>
                     {/* Sizes */}
                   </section>
                 </div>
